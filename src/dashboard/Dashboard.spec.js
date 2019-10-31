44363eecc0fe29159ab7eaf52e0f2bfdc153492d
Ian <mus5002@aol.com>
2019-10-30 21:27:15 -0400

    started writing test for component state

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,17 @@
 // Test away
+import React from 'react'
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Dashboard from './Dashboard'
+
+afterEach(rtl.cleanup);
+
+describe('Gate', () => {
+    const component = rtl.render(<Dashboard />)
+    
+    it('should default to unlocked and open ', () => {
+    
+        expect(component.instance().state)
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from 'react'
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Display from './Display'
+
+afterEach(rtl.cleanup);
+
+
